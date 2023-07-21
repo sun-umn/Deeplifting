afd5fe3ddfdb87150f2d2752d0a5d899fe4c724b
Ryan <ryan@tesorio.com>
2023-07-21 13:04:17 -0700

    making some fixes to the pipeline

--- deeplifting/optimization.py
+++ deeplifting/optimization.py
@@ -503,10 +503,10 @@ def run_deeplifting(problem: Dict, trials: int):
         set_seed(trial)
 
         model = DeepliftingMLP(
-            input_size=25,
+            input_size=100,
             # layer_sizes=(256, 256, 256, 256, 256),
             # layer_sizes=(512, 512, 512, 512, 512),
-            layer_sizes=(1024, 1024, 1024, 1024),
+            layer_sizes=(1024, 1024, 1024, 1024, 1024),
             output_size=dimensions,
         )
         model = model.to(device=device, dtype=torch.double)
