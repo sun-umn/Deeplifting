001a458f319f1129930fa26a68d974dc86730439
Ryan <ryan@tesorio.com>
2023-07-21 12:50:46 -0700

    adding and updating job and tasks

--- tasks.py
+++ tasks.py
@@ -1,12 +1,12 @@
-# stdlib
-import os
-
+#!/usr/bin/python
 # third party
 import click
 import neptune
-from optimization import run_deeplifting
-from problems import PROBLEMS_BY_NAME
-from utils import create_optimization_plot
+
+# first party
+from deeplifting.optimization import run_deeplifting
+from deeplifting.problems import PROBLEMS_BY_NAME
+from deeplifting.utils import create_optimization_plot
 
 
 @click.command('run-deeplifting')
@@ -25,22 +25,25 @@ def run_deeplifting_task(problem_name):
     # Get api token
     # TODO: If api token is not present log a warning
     # and default to saving files locally
-    neptune_api_token = os.environ.get('NEPTUNE_API_TOKEN')
-
     run = neptune.init_run(
         project="dever120/Deeplifting",
-        api_token=neptune_api_token,
-    )
+        api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiIzYmIwMTUyNC05YmZmLTQ1NzctOTEyNS1kZTIxYjU5NjY5YjAifQ==",  # noqa
+    )  # your credentials
 
     # Get the deeplifting outputs
     # TODO: Will make the arguments of the model configurable
     # So we can use hyperopt to test different configurations
     # of the model
-    outputs = run_deeplifting(problem, trials=10)
+    outputs = run_deeplifting(problem, trials=1)
 
     # Get final results
     results = outputs['final_results']
 
     # Log the image and results
     fig = create_optimization_plot(problem_name, problem, results, colormap='autumn_r')
-    run[f"deeplifting-{problem.name}-final-results-surface-and-contour"].upload(fig)
+    run[f"deeplifting-{problem_name}-final-results-surface-and-contour"].upload(fig)
+    fig.close()
+
+
+if __name__ == "__main__":
+    run_deeplifting_task()
