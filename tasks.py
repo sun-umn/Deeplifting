7b6627f55dfd0fb972aa2038cc94c16ab1cc8a10
Ryan <ryan@tesorio.com>
2023-08-10 06:55:17 -0700

    updating list and adding more time to slurm jobs

--- tasks.py
+++ tasks.py
@@ -21,24 +21,24 @@ from deeplifting.utils import create_contour_plot
 
 # Identify problems to run
 low_dimensional_problem_names = [
-    'ackley',  # Low
-    'bukin_n6',  # High, 2 layer is best so far, takes a while to run
-    'cross_in_tray',  # Low, runs quickly
-    'drop_wave',  # Low, runs quickly
-    'eggholder',  # Medium, takes time to run
-    'griewank',  # Low, (1.0 with 3-layer, 0.95 2-layer)
-    'holder_table',  # Medium
-    'levy',  # Low, 3-layer
-    'levy_n13',  # Low, 3-layer
-    'rastrigin',  # Low, 3-layer
-    'schaffer_n2',  # Low, 3-layer
-    'schaffer_n4',  # Low, 3-layer
-    'schwefel',  # takes a while to run
-    'shubert',
-    'ex8_1_1',
-    'kriging_peaks_red010',
-    'kriging_peaks_red020',
-    # 'kriging_peaks_red030',
+    # 'ackley',  # Low
+    # 'bukin_n6',  # High, 2 layer is best so far, takes a while to run
+    # 'cross_in_tray',  # Low, runs quickly
+    # 'drop_wave',  # Low, runs quickly
+    # 'eggholder',  # Medium, takes time to run
+    # 'griewank',  # Low, (1.0 with 3-layer, 0.95 2-layer)
+    # 'holder_table',  # Medium
+    # 'levy',  # Low, 3-layer
+    # 'levy_n13',  # Low, 3-layer
+    # 'rastrigin',  # Low, 3-layer
+    # 'schaffer_n2',  # Low, 3-layer
+    # 'schaffer_n4',  # Low, 3-layer
+    # 'schwefel',  # Takes a while to run, DA is better at 100% but we are at 85%
+    # 'shubert',  # Takes a while to run,
+    # 'ex8_1_1',
+    # 'kriging_peaks_red010',
+    # 'kriging_peaks_red020',
+    # # 'kriging_peaks_red030',
     'mathopt6',
     'quantum',
     'rosenbrock',
