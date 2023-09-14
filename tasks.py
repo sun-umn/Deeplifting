524a8485677ddf9d1bea2e7ed91539493d8ed98b
Ryan <ryan@tesorio.com>
2023-09-14 14:37:06 -0700

    adding all LD tasks

--- tasks.py
+++ tasks.py
@@ -27,7 +27,7 @@ from deeplifting.utils import create_contour_plot
 
 # Identify problems to run
 low_dimensional_problem_names = [
-    # 'ackley',  # Low
+    'ackley',  # Low
     'ackley2',
     'ackley3',
     'adjiman',
@@ -38,29 +38,26 @@ low_dimensional_problem_names = [
     'bird',
     'bohachevsky1',
     'bohachevsky2',
-    # 'bukin_n6',  # High, 2 layer is best so far, takes a while to run
-    # 'cross_in_tray',  # Low, runs quickly
-    # 'cross_leg_table',
-    # 'drop_wave',  # Low, runs quickly
-    # 'eggholder',  # Medium, takes time to run
-    # 'griewank',  # Low, (1.0 with 3-layer, 0.95 2-layer)
-    # 'holder_table',  # Medium
-    # 'levy',  # Low, 3-layer
-    # 'levy_n13',  # Low, 3-layer
-    # 'rastrigin',  # Low, 3-layer
-    # 'schaffer_n2',  # Low, 3-layer
-    # 'schaffer_n4',  # Low, 3-layer
-    # 'schwefel',  # Takes a while to run, DA is better at 100% but we are at 85%
-    # 'shubert',  # Takes a while to run
+    'booth',
+    'bukin_n6',  # High, 2 layer is best so far, takes a while to run
+    'cross_in_tray',  # Low, runs quickly
+    'cross_leg_table',
+    'drop_wave',  # Low, runs quickly
+    'eggholder',  # Medium, takes time to run
+    'griewank',  # Low, (1.0 with 3-layer, 0.95 2-layer)
+    'holder_table',  # Medium
+    'levy',  # Low, 3-layer
+    'levy_n13',  # Low, 3-layer
+    'rastrigin',  # Low, 3-layer
+    'schaffer_n2',  # Low, 3-layer
+    'schaffer_n4',  # Low, 3-layer
+    'schwefel',  # Takes a while to run, DA is better at 100% but we are at 85%
+    'shubert',  # Takes a while to run
     'ex8_1_1',
     'mathopt6',
     'rosenbrock',
     # 'kriging_peaks_red010',
     # 'kriging_peaks_red020',
-    # 'beale',
-    # 'bird',
-    # 'bohachevsky3',
-    # 'booth',
     # 'brent',
     # 'brown',
     # 'bukin_n2',
