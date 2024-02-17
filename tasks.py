f96b15ce5a6e47fcdf2bf151ce2d79d12e14e9f1
Ryan <ryan@tesorio.com>
2024-02-17 12:27:41 -0800

    will also try a bird run - try 16 cpu

--- tasks.py
+++ tasks.py
@@ -1500,7 +1500,7 @@ def run_deeplifting_pygranso_parallel(inputs):
 
 
 @cli.command('test-parallel')
-@click.option('--problem_name', default='alpine2')
+@click.option('--problem_name', default='bird')
 @click.option('--dimensionality', default='low-dimensional')
 @click.option('--experimentation', default=True)
 def test_parallel(
